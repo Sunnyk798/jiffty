575f58923255acb64b238644f1d724cdb0d6f54d
Gunjan Raj Tiwari <im.gunjan1@gmail.com>
2021-07-27 17:12:51 +0545

    💎 Login page created

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,11 @@ import "./App.css";
 import Topnav from "./components/Topnav";
 import Sidenav from "./components/Sidenav";
 import VideoList from "./components/VideoList";
+import Login from "./components/Login";
 
 function App() {
+	var loggedIn = false;
+	if (!loggedIn) return <Login />;
 	return (
 		<div className='App'>
 			<Topnav />
---
+++ client/src/components/Login.js
@@ -0,0 +1,10 @@
+import "./Login.css";
+
+export default function Login() {
+	return (
+		<div className='login-page'>
+			<div className='large-logo'>Jiffty</div>
+			<button className='login-btn'>Login with Google</button>
+		</div>
+	);
+}
