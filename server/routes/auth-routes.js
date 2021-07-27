778b1ca461637cebab2e044f2d4bf49da7d2c688
Sandesh Dhungana <dhunganasandesh32@gmail.com>
2021-07-27 16:41:30 +0545

    ✨ Made a simple callback URI after google authentication

--- server/routes/auth-routes.js
+++ server/routes/auth-routes.js
@@ -5,4 +5,8 @@ router.get("/google", passport.authenticate('google', {
     scope: ['profile']
 }));
 
+router.get("/google/redirect", (req, res) => {
+    res.send("Great! You are signed in. Tell me what your interests are!");
+})
+
 module.exports = router;
