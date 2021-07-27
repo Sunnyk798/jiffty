0f6c2556452bc17bfc77c1dbb5a1c2eee1816aba
Sandesh Dhungana <dhunganasandesh32@gmail.com>
2021-07-27 22:22:05 +0545

    🛡 Saved the user info to the database

--- server/config/passport-setup.js
+++ server/config/passport-setup.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
-require('dotenv').config();
+const User = require("../models/User");
 
 passport.use(new GoogleStrategy({
     callbackURL: '/auth/google/redirect',
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET
-}, () => {
-
+}, async(accessToken, refreshToken, profile, done) => {
+    const user = { name: profile.displayName, email: profile.emails[0].value };
+    await new User(user).save();
 }));
--- server/routes/auth-routes.js
+++ server/routes/auth-routes.js
@@ -2,10 +2,10 @@ const router = require("express").Router();
 const passport = require("passport")
 
 router.get("/google", passport.authenticate('google', {
-    scope: ['profile']
+    scope: ['profile', 'email']
 }));
 
-router.get("/google/redirect", (req, res) => {
+router.get("/google/redirect", passport.authenticate('google'), (req, res) => {
     res.send("Great! You are signed in. Tell me what your interests are!");
 })
 
