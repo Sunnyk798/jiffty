f894615262da74251df08a737b2793a0365abdbd
Gunjan Raj Tiwari <im.gunjan1@gmail.com>
2021-07-30 17:34:20 +0545

    💎  Upload form and validation

--- client/src/components/Upload.js
+++ client/src/components/Upload.js
@@ -1,30 +1,68 @@
-import { useState } from "react";
+import { useForm } from "react-hook-form";
 import "./Upload.css";
 
 export default function Upload() {
-	const [title, setTitle] = useState("");
-	const [description, setDescription] = useState("");
-	const [video, setVideo] = useState(null);
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
 
+	function submitHandler(data) {
+		console.log(data);
+	}
+
+	console.log(errors);
 	return (
 		<div className='upload'>
 			<h3>Upload video</h3>
-			<form>
-				<label to='video-upload'>Video </label>
-				<input type='file' className='file-upload' id='video-upload' />
+
+			<form onSubmit={handleSubmit(submitHandler)}>
+				<label htmlFor='video-upload'>Video </label>
+
+				<input
+					type='file'
+					accept='video/mp4,video/*'
+					className='file-upload'
+					id='video-upload'
+					{...register("video", {
+						required: "This field is required.",
+						maxLength: 150,
+					})}
+				/>
+
+				{errors.video && <small>{errors.video.message}</small>}
+
 				<br />
 
-				<label to='title-upload'>Title </label>
+				<label htmlFor='title-upload'>Title </label>
+
 				<input
 					type='text'
 					maxLength='200'
 					className='title-upload'
 					id='title-upload'
+					{...register("title", { required: "This field is required." })}
 				/>
+
+				{errors.title && <small>{errors.title.message}</small>}
+
 				<br />
 
-				<label to='desc-upload'>Description </label>
-				<textarea className='desc-upload'></textarea>
+				<label htmlFor='desc-upload'>Description </label>
+
+				<textarea
+					className='desc-upload'
+					id='desc-upload'
+					{...register("description", {
+						required: "This field is required.",
+					})}></textarea>
+
+				{errors.description && <small>{errors.description.message}</small>}
+
+				<br />
+
+				<input className='upload-btn' value='UPLOAD' type='submit' />
 			</form>
 		</div>
 	);
