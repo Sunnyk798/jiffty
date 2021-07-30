6126b68dd734b1f90425348ce99d7722d35dedc0
Gunjan Raj Tiwari <im.gunjan1@gmail.com>
2021-07-30 09:03:30 +0545

    💎 Proxy and backend connection

--- client/src/components/VideoList.js
+++ client/src/components/VideoList.js
@@ -1,7 +1,19 @@
+import { useState, useEffect } from "react";
 import VideoCard from "./VideoCard";
 import "./VideoList.css";
 
 export default function VideoList({ title }) {
+	const [videos, setVideos] = useState([]);
+
+	useEffect(() => {
+		fetch("/videos")
+			.then(res => res.json())
+			.then(result => {
+				console.log(result);
+				setVideos(result);
+			});
+	}, []);
+
 	return (
 		<div className='video-list'>
 			<div>{title}</div>
