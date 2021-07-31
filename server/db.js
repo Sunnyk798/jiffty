d10845a1ba4f584bd2f4443e6ed80125016adb16
Gunjan Raj Tiwari <im.gunjan1@gmail.com>
2021-07-31 11:39:09 +0545

    🎨 file preview, custom upload button

--- server/db.js
+++ server/db.js
@@ -2,14 +2,16 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 module.exports = function connectDB() {
-    mongoose.connect(
-        process.env.MONGO_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        },
-        err => {
-            if (err) console.log("DB connection failed...");
-            else console.log("DB connected ...");
-        }
-    );
+	mongoose.connect(
+		process.env.MONGO_URL,
+		{
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+		},
+		err => {
+			if (err) console.log("DB connection failed...");
+			else console.log("DB connected ...");
+		}
+	);
 };
