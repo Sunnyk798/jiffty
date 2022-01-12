bb5f215c0aa4e688aba595d575ad6ca463c409ca
Gunjan Raj Tiwari <im.gunjan1@gmail.com>
2022-01-12 12:35:35 +0545

    Profile button styled✨

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -39,7 +39,7 @@ export default function Profile({ user }) {
                         <span> {profile.following.length}</span> Following
                     </div>
                     {
-                        (user._id == profile._id) ? (
+                        (user._id === profile._id) ? (
                             <button className="grey-btn">Edit Profile</button>
                         ) : (
                             isFollowing ? 
